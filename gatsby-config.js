93ad3dbdfd1ffd9d1e36f3617c7559d119ca0248
Gregor Majcen <gregor.majcen@sinergise.com>
2020-08-25 13:22:42 +0200

    gatsby-remark-embed-snippet

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          `gatsby-remark-embed-snippet`,
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
